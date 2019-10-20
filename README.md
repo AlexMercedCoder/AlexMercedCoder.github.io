73e3df32ca5cd215a24fabdda1fd827244a4b1aa
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-10-20 19:58:05 -0400

    redid portfolio website

---
+++ README.md
@@ -0,0 +1,34 @@
+# NAME OF PROJECT
+## By Author
+### [LIVE PROJECT](https://www.AlexMercedCoder.com)
+### [Portfolio Website](https://www.AlexMercedCoder.com)
+---
+**SUMMARY** :
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+---
+## Resources Used
+
+- jquery
+- jquery UI
+- Google Fonts (Font A, Font B)
+- Other CSS and JS Libraries
+
+## Design of Functionality
+
+Explanation of my code
+
+```
+CODE TO BE EXPLAINED
+```
+
+
+
+## Other Comments
+
+- I had these problems that I had to work around
+- I wanted to add these features but couldn't because
+
+
+<!-- Image Tag: ![alt text](image.jpg) -->
+<!-- Link Tag: [title](https://www.example.com) -->
+<!-- https://www.markdownguide.org/cheat-sheet/ -->
---
+++ js/app.js
@@ -0,0 +1,93 @@
+//added the defer keyword to my script tag, to avoid script pre-loading problems
+
+
+///////////////////////////////////////
+//PROGRAM NAME:
+//PROGRAM BY
+///////////////////////////////////////
+
+
+//GLOBAL DATA SETS
+
+
+const gVals = {
+
+
+
+}
+
+
+
+// CLASSES
+
+
+
+
+
+//FUNCTIONS DEFINITIONS
+
+const myFuncs = {
+
+    //////////////////////
+    //random number function
+    //by Alex Merced
+    ////////////////////////
+    randNum: (num) => {
+
+        return Math.floor(Math.random()*(num+1));
+
+    },
+
+    ///////////////////
+    //random number in a range function
+    //by Alex Merced
+    ///////////////////
+
+    randRange: (floor,ceiling) => {
+        let num = 0;
+        while(num < floor || num > ceiling){
+            num = randNum(ceiling);
+
+        }
+        return num;
+    },
+
+    unlockSecret: () => {
+
+        $('header').append(domVals.$secretDiv);
+
+    }
+}
+
+
+//DOM VARIABLES
+
+const domVals = {
+
+
+    $secretDiv: $('<div>').addClass('secret').html(`
+    
+    <br><iframe width="40%" height="315" src="https://www.youtube.com/embed/nawRt_xO9U4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p> My name is Alex Merced and I'm a Full Stack Developer who brings a creative and curious outlook to the table along with experience in education, finance, broadcast, retail and politics to contribute unique ideas and perspectives.</p>
+    <p>In broadcasting, finance and politics I've been known as an effective educator and communicator whether speaking/training in front of voters, candidates, new hires and managing directors. As a full stack developer I hope to also be a top-tier educator, communicator and a top-tier creator using my ability to learn, adapt and deliver effectively.</p>
+    <a href="https://www.youtube.com/channel/UCoc4UCEetAt3htM3hV1dQgQ"><button>Alex Explains CS Concepts</button></a>
+    <a href="https://open.spotify.com/show/1kMcquypdIElTu4Nu98XPM"><button>Alex's Web Dev 101 Podcast</button></a>
+    <a href="https://git.generalassemb.ly/AlexMerced"><button>Alex's Other Skills</button></a>
+    <a href="https://www.linkedin.com/in/alexmerced"><button>Resume/LinkedIn</button></a>
+    
+    `),
+
+
+
+}
+
+
+
+//EVENT LISTENERS
+
+
+$('.unlock').on("click", myFuncs.unlockSecret);
+
+
+
+//THE PROGRAM
