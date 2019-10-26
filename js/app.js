25a1e3759f11ea7e6ffc4a8b00805523a5025e36
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-10-26 09:43:14 -0400

    minor word changes

--- js/app.js
+++ js/app.js
@@ -68,8 +68,8 @@ const domVals = {
     $secretDiv: $('<div>').addClass('secret').html(`
     
     <br><iframe width="40%" height="315" src="https://www.youtube.com/embed/nawRt_xO9U4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <p> My name is Alex Merced and I'm a Full Stack Developer who brings a creative and curious outlook to the table along with experience in education, finance, broadcast, retail and politics to contribute unique ideas and perspectives.</p>
-    <p>In broadcasting, finance and politics I've been known as an effective educator and communicator whether speaking/training in front of voters, candidates, new hires and managing directors. As a full stack developer I hope to also be a top-tier educator, communicator and a top-tier creator using my ability to learn, adapt and deliver effectively.</p>
+    <p> My name is Alex Merced and I'm a Web Developer who brings a creative and curious outlook to the table along with experience in education, finance, broadcast, retail and politics to contribute unique ideas and perspectives.</p>
+    <p>In broadcasting, finance and politics I've been known as an effective educator and communicator whether speaking/training in front of voters, candidates, new hires and managing directors. As a web developer I hope to also be a quality educator, communicator and a top-tier creator using my ability to learn, adapt and deliver effectively.</p>
     <a href="https://www.youtube.com/channel/UCoc4UCEetAt3htM3hV1dQgQ"><button>Alex Explains CS Concepts</button></a>
     <a href="https://open.spotify.com/show/1kMcquypdIElTu4Nu98XPM"><button>Alex's Web Dev 101 Podcast</button></a>
     <a href="https://git.generalassemb.ly/AlexMerced"><button>Alex's Other Skills</button></a>
